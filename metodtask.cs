3b53f80f06f2929ce10bfcd3785688712a2f641f
gamzaat <gamzaat@code.edu.az>
2023-02-27 18:11:19 +0400

    methodTask

---
+++ metodtask.cs
@@ -0,0 +1,44 @@
+﻿#region task2
+using System;
+
+int Custom(params int[] arr)
+{
+    int max = arr[0];
+    for (int i = 1; i < arr.Length; i++)
+    {
+        int num = arr[i];
+        if (max < num)
+        {
+            max = num;
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    
+        } 
+
+    }
+    return max;
+
+};
+
+
+ int result = Custom( 2, 3, 4);
+Console.WriteLine(result);
+#endregion
+#region task1
+int[] arr = { 6, 12, 30, 45, 67, 97 };
+
+sum(12);
+void sum(int num)
+{
+    for (int i = 0; i < arr.length; i++)
+    {
+        if (arr[i] == num)
+        {
+            console.writeline("var");
+        }
+        else
+        {
+            console.writeline("yoxdu");
+        }
+
+    }
+}
+#endregion
